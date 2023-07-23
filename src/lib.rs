fa78ffb47c2953ec9fa7357d2dee0e4ae7f033da
Sapiet1 <3cvang24@gmail.com>
2023-07-23 13:35:14 -0500

    added documentation and README

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,56 @@
+//! A tiny crate for error handling. It is able to convert items of the 
+//! [`Error`](https://doc.rust-lang.org/std/error/trait.Error.html) trait into
+//! their messages, allowing for easy propagation. 
+//!
+//! # Examples
+//! 
+//! ```rust
+//! use tiny_error::ErrorMessage;
+//! 
+//! use std::{
+//!     env,
+//!     fs,
+//!     path::PathBuf,
+//! };
+//! 
+//! fn main() -> Result<(), ErrorMessage> {
+//!     // Text when failed:
+//!     // Error: Invalid input
+//!     // Correct Usage: `[crate_name] example/file/path.txt`
+//!     let path = get_path()?;
+//!     // Text when failed: 
+//!     // Error: No such file or directory (os error 2)
+//!     let file = fs::read_to_string(path)?;
+//! 
+//!     Ok(())
+//! }
+//! 
+//! // Gets the first argument passed. If none or more were, returns an
+//! // `ErrorMessage`.
+//! fn get_path() -> Result<PathBuf, ErrorMessage> {
+//!     let mut args = env::args().skip(1);
+//!     let arg = args.next().filter(|_| args.next().is_none());    
+//! 
+//!     arg
+//!         .map(|input| input.into())
+//!         .ok_or_else(|| ErrorMessage::new(
+//!             "Invalid input\n\
+//!              Correct Usage: `[crate_name] example/file/path.txt`"
+//!         ))
+//! }
+//! ```
+
 use std::{
     error::Error,
     fmt::{Debug, Formatter, Result as FmtResult},
 };
 
+/// The struct that holds the error message.
 pub struct ErrorMessage(String);
 
 impl ErrorMessage {
+    /// Constructs a new [`ErrorMessage`] with the passed parameter,
+    /// converting it into a [`String`](https://doc.rust-lang.org/std/string/struct.String.html).
     pub fn new<S: Into<String>>(message: S) -> Self {
         ErrorMessage(message.into())
     }
